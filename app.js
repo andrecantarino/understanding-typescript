ae54d35bae4d66bf28a20ccdbc81edd51f6697b2
andre cantarino <cantarino.andre@gmail.com>
2022-06-05 16:09:10 -0300

    array types

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var person = {
     name: "Andre",
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'cooking']
 };
+/** any[] will define the array of different types */
+var anyArray;
+anyArray = ['sports', 1];
+var favoriteActivities;
+favoriteActivities = ['sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
 const person: {
   name: string,
-  age: number
+  age: number,
+  hobbies: string[],
 } = {
   name: "Andre",
-  age: 30
+  age: 30,
+  hobbies: ['sports', 'cooking']
 }
 
+/** any[] will define the array of different types */
+let anyArray: any[];
+anyArray = ['sports', 1];
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
