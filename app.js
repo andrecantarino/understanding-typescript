6d013a1fd838b5232c5ad158add51276e37c2acd
andre cantarino <cantarino.andre@gmail.com>
2022-06-05 16:24:15 -0300

    tuples

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var person = {
     name: "Andre",
     age: 30,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+/** ARRAYS */
 /** any[] will define the array of different types */
 var anyArray;
 anyArray = ['sports', 1];
 var favoriteActivities;
 favoriteActivities = ['sports'];
+/** TUPLES */
+person.role.push('admin');
+// person.role[1] = 10;
+/** PRINTS */
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
--- app.ts
+++ app.ts
@@ -2,12 +2,15 @@ const person: {
   name: string,
   age: number,
   hobbies: string[],
+  role: [number, string] // tuple
 } = {
   name: "Andre",
   age: 30,
-  hobbies: ['sports', 'cooking']
+  hobbies: ['sports', 'cooking'],
+  role: [2, 'author'],
 }
 
+/** ARRAYS */
 /** any[] will define the array of different types */
 let anyArray: any[];
 anyArray = ['sports', 1];
@@ -15,6 +18,11 @@ anyArray = ['sports', 1];
 let favoriteActivities: string[];
 favoriteActivities = ['sports'];
 
+/** TUPLES */
+person.role.push('admin');
+// person.role[1] = 10;
+
+/** PRINTS */
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
