4d7047f7176a24dcbcbd97ec813019dd591336c6
andre cantarino <cantarino.andre@gmail.com>
2022-06-05 16:35:15 -0300

    enum

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+/** EMUN */
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
+})(Role || (Role = {}));
+;
+/** OBJECT */
+// const person: {
+//   name: string,
+//   age: number,
+//   hobbies: string[],
+//   role: [number, string] // tuple
+// } = {
+//   name: "Andre",
+//   age: 30,
+//   hobbies: ['sports', 'cooking'],
+//   role: [2, 'author'],
+// }
 var person = {
     name: "Andre",
     age: 30,
     hobbies: ['sports', 'cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 /** ARRAYS */
 /** any[] will define the array of different types */
@@ -11,11 +34,14 @@ anyArray = ['sports', 1];
 var favoriteActivities;
 favoriteActivities = ['sports'];
 /** TUPLES */
-person.role.push('admin');
+// person.role.push('admin');
 // person.role[1] = 10;
 /** PRINTS */
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('ADMIN');
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,28 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[],
-  role: [number, string] // tuple
-} = {
+/** EMUN */
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+
+/** OBJECT */
+// const person: {
+//   name: string,
+//   age: number,
+//   hobbies: string[],
+//   role: [number, string] // tuple
+// } = {
+//   name: "Andre",
+//   age: 30,
+//   hobbies: ['sports', 'cooking'],
+//   role: [2, 'author'],
+// }
+
+const person = {
   name: "Andre",
   age: 30,
   hobbies: ['sports', 'cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 }
 
 /** ARRAYS */
@@ -19,7 +34,7 @@ let favoriteActivities: string[];
 favoriteActivities = ['sports'];
 
 /** TUPLES */
-person.role.push('admin');
+// person.role.push('admin');
 // person.role[1] = 10;
 
 /** PRINTS */
@@ -27,5 +42,9 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('ADMIN');
 }
 
