bfa40ada568a34bc7a4db918781bedb387b71922
andre cantarino <cantarino.andre@gmail.com>
2022-06-11 11:54:45 -0300

    function return types and void

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-var combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
-// const number1 = 5;
-// const number2 = 2.8;
-// const printResult = true;
-// const resultPhrase = 'Result is: '
-// add(number1, number2, printResult, resultPhrase);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,40 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-    input1: Combinable,
-    input2: Combinable,
-    resultConversion: ConversionDescriptor
-  ) {
-  let result;
-
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
-
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function printResult(num: number): void {
+  console.log('Result: '+ num);
+}
 
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-
-const combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
-
-// const number1 = 5;
-// const number2 = 2.8;
-// const printResult = true;
-// const resultPhrase = 'Result is: '
-
-// add(number1, number2, printResult, resultPhrase);
+printResult(add(5, 12));
